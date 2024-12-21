b221b10525d3949dfa4e43fe11f3cf1462b5b93c
avoiddeveloper <karimwdd03@gmail.com>
2024-12-21 23:34:04 +0600

    navbar and category tabs part work done

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,26 @@
-console.log('Hello from js');
+const loadAndShowCategories = async () => {
+    const categoryContainer = document.getElementById('category_container');
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
+        const data = await response.json();
+        data.categories.forEach(category => {
+            // console.log(category)
+            const btn = document.createElement('button');
+            btn.classList = 'btn font-semibold text-lg';
+            btn.innerText = category.category;
+            categoryContainer.append(btn);
+        });
+    }
+    catch(err){
+        alert('Data Fetching Error');
+    }
+}
+
+
+
+
+
+
+
+
+loadAndShowCategories();
